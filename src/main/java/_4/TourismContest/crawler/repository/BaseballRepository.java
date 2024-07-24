99da9f24a1e2f98a3579100cf76eb3bd69b0d00a
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-07-24 12:18:26 +0900

    Feat/#1 (#9)
    * Feat: CICD 설정
    
    * Feat: CICD Test
    
    * Feat: 스프링 스케줄러 적용
    
    * Feat: 야구경기 일정 크롤링 스케줄러 적용

--- src/main/java/_4/TourismContest/crawler/repository/BaseballRepository.java
+++ src/main/java/_4/TourismContest/crawler/repository/BaseballRepository.java
@@ -2,6 +2,8 @@ package _4.TourismContest.crawler.repository;
 
 import _4.TourismContest.crawler.domain.Baseball;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.Optional;
 
 @Repository
 public interface BaseballRepository extends JpaRepository<Baseball,Long> {
-    Optional<List<Baseball>> findAllByMonthAndDate(int month, int date);
-    Optional<Baseball> findByMonthAndDateAndTimeAndAwayAndHomeAndLocation(int month, int date, String time,String home, String away, String location);
+//    Optional<List<Baseball>> findAllByMonthAndDate(int month, int date);
+
+    @Query("SELECT b FROM Baseball b WHERE b.month = :month AND b.date = :date AND b.time = :time AND b.home = :home AND b.away = :away AND b.location = :location")
+    Optional<Baseball> findByMonthAndDateAndTimeAndAwayAndHomeAndLocation(@Param("month") int month, @Param("date") int date, @Param("time") String time, @Param("home") String home, @Param("away") String away, @Param("location") String location);
 }
--- src/main/java/_4/TourismContest/crawler/scheduler/BaseballScheduler.java
+++ src/main/java/_4/TourismContest/crawler/scheduler/BaseballScheduler.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class BaseballScheduler {
