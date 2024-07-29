c8eadb560a442d62c45537d938a0a65081583256
m0304s <cms8955@naver.com>
2024-07-29 14:38:17 +0900

    Feat: 야구 경기 일정 컬럼 병합

--- src/main/java/_4/TourismContest/crawler/domain/Baseball.java
+++ src/main/java/_4/TourismContest/crawler/domain/Baseball.java
@@ -4,6 +4,8 @@ package _4.TourismContest.crawler.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -11,26 +13,20 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @ToString
-@EqualsAndHashCode(of = {"date", "month", "home", "away", "time"})
+@EqualsAndHashCode(of = {"home", "away", "time"})
 @Table(name = "Baseball")
 public class Baseball {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String time;
+    private LocalDateTime time;
     private String home;
     private String away;
     private String location;
     private String status;
-    @Column(nullable = true)
     private String homePitcher;
-    @Column(nullable = true)
     private String awayPitcher;
-    private int month;
-    private int date;
-    @Column(nullable = true)
     private int homeScore;
-    @Column(nullable = true)
     private int awayScore;
     private String weekDay;
 }
