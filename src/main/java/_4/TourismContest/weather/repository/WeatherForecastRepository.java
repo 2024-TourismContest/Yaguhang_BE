f8489ff86ea9f8c47bd50193c10f086f9a13ac14
m0304s <cms8955@naver.com>
2024-07-31 17:17:09 +0900

    Feat: 날씨 API 호출 시간 변경

--- src/main/java/_4/TourismContest/weather/repository/WeatherForecastRepository.java
+++ src/main/java/_4/TourismContest/weather/repository/WeatherForecastRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface WeatherForecastRepository extends JpaRepository<WeatherForecast, Long> {
-    List<WeatherForecast> findByFcstDateAndFcstTimeAndCategory(String fcstDate, String fcstTime, String category);
+    List<WeatherForecast> findByNxAndNyAndFcstDateAndFcstTimeAndCategory(int nx, int ny, String fcstDate, String fcstTime, String category);
+//    List<WeatherForecast> findByNxAndNyFcstDateAndFcstTimeAndCategory(int nx, int ny, String fcstDate, String fcstTime, String category);
 }
