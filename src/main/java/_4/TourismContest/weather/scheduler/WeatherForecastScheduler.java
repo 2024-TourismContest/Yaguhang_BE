afc462a28e780ff23f8a3672ffa877cf61b572c8
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-07-31 16:46:11 +0900

    Feat/#20 (#25)
    * Feat: 기상청 API 연결 및 스케줄러 설정
    
    * Feat: @Transactional 어노테이션 추가
    
    * Feat: Service Key @Value 적용
    
    * Feat: 모든 경기장의 날씨 데이터 수집

--- src/main/java/_4/TourismContest/weather/scheduler/WeatherForecastScheduler.java
+++ src/main/java/_4/TourismContest/weather/scheduler/WeatherForecastScheduler.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class WeatherForecastScheduler {
@@ -17,8 +19,25 @@ public class WeatherForecastScheduler {
 
     @Scheduled(cron = "0 0/30 * * * ?")
     public void fetchForecastData() throws IOException {
-        LocalDateTime now = LocalDateTime.now();
 
+        int[][] stadium = new int[13][2];
+
+        // 각 야구장의 좌표 추가
+        stadium[0] = new int[]{Math.round(37.5156f), Math.round(127.0730f)}; // 잠실야구장
+        stadium[1] = new int[]{Math.round(37.2859f), Math.round(127.0464f)}; // 수원 케이티 위즈 파크
+        stadium[2] = new int[]{Math.round(37.4352f), Math.round(126.6857f)}; // 인천 SSG 랜더스 필드
+        stadium[3] = new int[]{Math.round(35.2375f), Math.round(128.6811f)}; // 창원 NC파크
+        stadium[4] = new int[]{Math.round(35.1796f), Math.round(126.8786f)}; // 광주-기아 챔피언스필드
+        stadium[5] = new int[]{Math.round(35.1944f), Math.round(129.0617f)}; // 사직야구장
+        stadium[6] = new int[]{Math.round(35.8413f), Math.round(128.6812f)}; // 대구 삼성 라이온즈 파크
+        stadium[7] = new int[]{Math.round(36.3174f), Math.round(127.4278f)}; // 대전 한화생명 이글스파크
+        stadium[8] = new int[]{Math.round(37.4971f), Math.round(126.8679f)}; // 고척 스카이돔
+        stadium[9] = new int[]{Math.round(35.9670f), Math.round(126.7184f)}; // 월명종합경기장 야구장
+        stadium[10] = new int[]{Math.round(35.5422f), Math.round(129.2560f)}; // 울산 문수 야구장
+        stadium[11] = new int[]{Math.round(36.0190f), Math.round(129.3715f)}; // 포항 야구장
+        stadium[12] = new int[]{Math.round(36.6431f), Math.round(127.4917f)}; // 청주 야구장
+
+        LocalDateTime now = LocalDateTime.now();
         LocalDateTime previousHour = now.minusHours(1);
 
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
@@ -27,10 +46,10 @@ public class WeatherForecastScheduler {
         String baseDate = previousHour.format(dateFormatter);
         String baseTime = previousHour.format(timeFormatter);
 
-        System.out.println("baseTime = " + baseTime);
-        int nx = 55; // 경기장 X 좌표
-        int ny = 127; // 경기장 Y 좌표
-
-        weatherForecastService.fetchAndSaveForecastData(baseDate, baseTime, nx, ny);
+        for (int[] point : stadium) {
+            int nx = point[0]; // 경기장 X 좌표
+            int ny = point[1]; // 경기장 Y 좌표
+            weatherForecastService.fetchAndSaveForecastData(baseDate, baseTime, nx, ny);
+        }
     }
 }
