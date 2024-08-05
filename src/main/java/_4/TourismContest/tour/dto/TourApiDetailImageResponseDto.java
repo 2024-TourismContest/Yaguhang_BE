3f5f51c81defe58cc431cac65592b079131135be
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-08-05 17:00:32 +0900

    Feat/#61 (#69)
    * Feat : 상세페이지 api
    
    * Feat/#61

--- src/main/java/_4/TourismContest/tour/dto/TourApiDetailImageResponseDto.java
+++ src/main/java/_4/TourismContest/tour/dto/TourApiDetailImageResponseDto.java
@@ -2,6 +2,7 @@
 
     import com.fasterxml.jackson.annotation.JsonProperty;
     import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+    import lombok.AllArgsConstructor;
     import lombok.Getter;
     import lombok.Setter;
 
@@ -23,10 +24,14 @@
         }
 
         @Getter
+        @Setter
         public static class Items {
             @JsonProperty("item")
             private List<Item> item;
 
+        }
+        @Getter
+        @AllArgsConstructor
             public void setItem(List<Item> item) {
                 this.item = item;
             }
