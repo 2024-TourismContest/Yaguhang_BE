e86fb12a3d0da24c7b9aa27eee5af2d47b872a01
m0304s <cms8955@naver.com>
2024-08-05 22:53:36 +0900

    fix: Scheduler 수정

--- src/main/java/_4/TourismContest/baseball/scheduler/BaseballScheduler.java
+++ src/main/java/_4/TourismContest/baseball/scheduler/BaseballScheduler.java
@@ -17,19 +17,19 @@ public class BaseballScheduler {
 
     @Autowired
     private BaseballService baseballService;
-//    @PostConstruct
-//    public void init() {
-//
-//        try {
-//            baseballService.scrapeAllSchedule();
-//        } catch (DataIntegrityViolationException e) {
-//            // Handle unique constraint violations and other data integrity issues
-//            logger.error("Data integrity violation occurred during PostConstruct initialization", e);
-//        } catch (Exception e) {
-//            // Handle any other unexpected exceptions
-//            logger.error("An unexpected error occurred during PostConstruct initialization", e);
-//        }
-//    }
+    @PostConstruct
+    public void init() {
+
+        try {
+            baseballService.scrapeAllSchedule();
+        } catch (DataIntegrityViolationException e) {
+            // Handle unique constraint violations and other data integrity issues
+            logger.error("Data integrity violation occurred during PostConstruct initialization", e);
+        } catch (Exception e) {
+            // Handle any other unexpected exceptions
+            logger.error("An unexpected error occurred during PostConstruct initialization", e);
+        }
+    }
 
     @Scheduled(cron = "0 0,30 0,13-23 * * *") // Every 30 minutes excluding 01:00-12:00
     public void scrapeGames() {
