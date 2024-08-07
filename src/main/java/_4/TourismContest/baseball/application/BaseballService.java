36b7a957fa798b32b5f875d5624e23e4ddf102bf
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-08-08 03:22:25 +0900

    Feat/#85 (#87)
    * feat : 경기 일정 구단 이름 줄 바꿈 처리
    
    * feat : 줄 바꿈 수정

--- src/main/java/_4/TourismContest/baseball/application/BaseballService.java
+++ src/main/java/_4/TourismContest/baseball/application/BaseballService.java
@@ -597,9 +597,9 @@ public class BaseballService {
         List<BaseBallDTO> baseballSchedules = baseballPage.getContent().stream()
                 .map(baseball -> {
                     String homeTeam = exchangeTeamName(baseball.getHome());
-                    String homeTeamOut = homeTeam.replace(" ", "\n");
+                    String homeTeamOut = homeTeam.replace(" ", "<br>");
                     String awayTeam = exchangeTeamName(baseball.getAway());
-                    String awayTeamOut = awayTeam.replace(" ", "\n");
+                    String awayTeamOut = awayTeam.replace(" ", "<br>");
                     WeatherForecastEnum weatherForecast = weatherForecastService.getWeatherForecastDataWithGame(baseball);
 
                     return BaseBallDTO.builder()
