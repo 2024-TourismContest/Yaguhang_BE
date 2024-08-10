a3252fe556c27a7b5f3c354bc9955c4a7ca9ae9b
JongKyeong Kim <oo7bangjk@naver.com>
2024-08-10 11:21:10 +0900

    fix : 경기 일정 스크랩 통합

--- src/main/java/_4/TourismContest/baseball/presentation/BaseballScrapController.java
+++ src/main/java/_4/TourismContest/baseball/presentation/BaseballScrapController.java
@@ -9,6 +9,7 @@ import _4.TourismContest.oauth.application.CurrentUser;
 import _4.TourismContest.oauth.application.UserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,21 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseballScrapController {
     private final BaseballScrapService baseballScrapService;
 
-    @PatchMapping("/schedule/create")
+    @PatchMapping("/schedule/scrap")
     @PreAuthorize("hasRole('USER')")
     @Operation(summary = "야구 경기 스크랩하기")
-    public ResponseEntity<BaseBallScrapResponseDTO> createScrap(@CurrentUser UserPrincipal userPrincipal, Long gameId){
-        BaseBallScrapResponseDTO scrap = baseballScrapService.createScrap(userPrincipal, gameId);
-        return ResponseEntity.ok(scrap);
+    public ResponseEntity<String> scrapSchdule(@CurrentUser UserPrincipal userPrincipal, Long gameId){
+        return new ResponseEntity<>(baseballScrapService.scrapSchdule(userPrincipal, gameId), HttpStatus.OK);
     }
 
-    @PatchMapping("/schedule/delete")
-    @PreAuthorize("hasRole('USER')")
-    @Operation(summary = "스크랩 되어 있는 경기 스크랩 취소하기")
-    public ResponseEntity<BaseBallScrapResponseDTO> deleteScrap(@CurrentUser UserPrincipal userPrincipal, Long gameId){
-        BaseBallScrapResponseDTO scrap = baseballScrapService.deleteScrap(userPrincipal, gameId);
-        return ResponseEntity.ok(scrap);
-    }
 
     @GetMapping("/schedule")
     @Operation(summary = "스크랩 한 경기 일정 조회하기", description = "내가 스크랩한 경기 일정 가져오기")
