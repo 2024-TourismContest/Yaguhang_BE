8308db9d567e091145836248561f7e25952bdd98
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-08-10 14:51:19 +0900

    fix : 기존 api 파라미터 수정 (#124)

--- src/main/java/_4/TourismContest/recommend/presentation/RecommendController.java
+++ src/main/java/_4/TourismContest/recommend/presentation/RecommendController.java
@@ -9,19 +9,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/recommend")
 @RequiredArgsConstructor
 public class RecommendController {
     private final RecommendService recommendService;
-    @GetMapping("/{stadiumId}/{pageSize}")
+    @GetMapping("")
     @Operation(summary = "유저가 추천하는 놀거리를 좋아요 순으로 가져오는 api" ,description = "-")
-    public ResponseEntity<RecommendPreviewResponse> getStadiumSpot(@PathVariable Long stadiumId, @PathVariable Integer pageSize,
+    public ResponseEntity<RecommendPreviewResponse> getStadiumSpot(@RequestParam Long stadiumId,
+                                                                   @RequestParam(defaultValue = "10") Integer pageSize,
                                                                    @CurrentUser UserPrincipal userPrincipal) {
         RecommendPreviewResponse recommendPreviewResponse = recommendService.getRecommendList(stadiumId, pageSize, userPrincipal);
         return new ResponseEntity<>(recommendPreviewResponse, HttpStatus.OK);
