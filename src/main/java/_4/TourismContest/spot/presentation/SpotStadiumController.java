d075efac53861aa24db85291c0f2f08c8906821d
JongKyeong Kim <oo7bangjk@naver.com>
2024-08-10 14:50:33 +0900

    fix : 기존 api 파라미터 수정

--- src/main/java/_4/TourismContest/spot/presentation/SpotStadiumController.java
+++ src/main/java/_4/TourismContest/spot/presentation/SpotStadiumController.java
@@ -11,10 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -25,11 +22,14 @@ import java.util.List;
 public class SpotStadiumController {
     private final SpotService spotService;
 
-    @GetMapping("/{stadiumId}/{category}/{pagesize}/{pageindex}/{radius}")
+    @GetMapping("")
     @Operation(summary = "구장별 보기 페이지 기본 카테고리 조회" ,description = "구장, 카테고리 필터를 사용하여, 조회. 페이지 사이즈 지정하여 원하는 데이터 개수 선택 가능합니다. 토큰과 같이 보낼 시 스크랩 여부도 같이 감. 카테고리 필터 : (숙소, 맛집, 문화, 쇼핑)\n radius (검색 범위) : Km 단위 (1km) ")
-    public ResponseEntity<SpotStadiumPreviewResponse> getStadiumSpot(@PathVariable Long stadiumId, @PathVariable String category,
-                                                                     @PathVariable Integer pagesize, @PathVariable Integer pageindex,
-                                                                     @PathVariable Integer radius, @CurrentUser UserPrincipal userPrincipal) {
+    public ResponseEntity<SpotStadiumPreviewResponse> getStadiumSpot(@RequestParam Long stadiumId,
+                                                                     @RequestParam String category,
+                                                                     @RequestParam(defaultValue = "5")  Integer pagesize,
+                                                                     @RequestParam(required = false)  Integer pageindex,
+                                                                     @RequestParam(defaultValue = "10")  Integer radius,
+                                                                     @CurrentUser UserPrincipal userPrincipal) {
         SpotStadiumPreviewResponse spotStadiumPreviewResponse = spotService.getStadiumSpot(stadiumId, category, pagesize, pageindex, radius,userPrincipal);
         return new ResponseEntity<>(spotStadiumPreviewResponse, HttpStatus.OK);
     }
