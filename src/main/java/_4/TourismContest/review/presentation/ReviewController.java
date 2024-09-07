07b99e43f8e09b10dfcb7eaf06591c67e18f7008
FhRh <yiyop@naver.com>
2024-09-07 15:34:15 +0900

    feat : 리뷰 관련 기능 userPrincipal 적용 완료

--- src/main/java/_4/TourismContest/review/presentation/ReviewController.java
+++ src/main/java/_4/TourismContest/review/presentation/ReviewController.java
@@ -1,6 +1,8 @@
 package _4.TourismContest.review.presentation;
 
 
+import _4.TourismContest.oauth.application.CurrentUser;
+import _4.TourismContest.oauth.application.UserPrincipal;
 import _4.TourismContest.review.application.ReviewService;
 import _4.TourismContest.review.dto.request.ReviewCreateRequest;
 import _4.TourismContest.review.dto.request.ReviewUpdateRequest;
@@ -9,7 +11,16 @@ import _4.TourismContest.review.dto.response.ReviewsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/review")
@@ -19,9 +30,9 @@ public class ReviewController {
 
     @PostMapping("/{spotId}")
     public ResponseEntity<String> saveReview(@PathVariable("spotId") Long spotId,
-                                       @RequestParam Long userId,
+                                       @CurrentUser UserPrincipal user,
                                        @RequestBody ReviewCreateRequest request) {
-        reviewService.saveReview(userId, spotId, request);
+        reviewService.saveReview(user.getId(), spotId, request);
         return new ResponseEntity<>("success save review", HttpStatus.OK);
     }
 
@@ -34,29 +45,29 @@ public class ReviewController {
 
     @PutMapping("/{reviewId}")
     public ResponseEntity<String> updateReview(@PathVariable("reviewId") Long reviewId,
-                                                @RequestParam Long userId,
+                                               @CurrentUser UserPrincipal user,
                                                 @RequestBody ReviewUpdateRequest request) {
-        reviewService.updateReview(reviewId, userId,request);
+        reviewService.updateReview(reviewId, user.getId(),request);
         return new ResponseEntity<>("success update review", HttpStatus.OK);
     }
 
     @DeleteMapping("/{reviewId}")
     public ResponseEntity<String> deleteReview(@PathVariable("reviewId") Long reviewId,
-                                       @RequestParam Long userId) {
-        reviewService.deleteReview(reviewId, userId);
+                                               @CurrentUser UserPrincipal user) {
+        reviewService.deleteReview(reviewId, user.getId());
         return new ResponseEntity<>("success delete review", HttpStatus.OK);
     }
 
     @PatchMapping("/{reviewId}/like")
     public ResponseEntity<String> likeReview(@PathVariable("reviewId") Long reviewId,
-                                               @RequestParam Long userId) {
-        String result = reviewService.likeReview(reviewId, userId);
+                                             @CurrentUser UserPrincipal user) {
+        String result = reviewService.likeReview(reviewId, user.getId());
         return new ResponseEntity<>("success "+result+" review", HttpStatus.OK);
     }
 
     @GetMapping("/my-review")
-    public ResponseEntity<ReviewPreviewsResponse> getReviews(@RequestParam Long userId) {
-        ReviewPreviewsResponse reviewsResponse = reviewService.getReviewsByUser(userId);
+    public ResponseEntity<ReviewPreviewsResponse> getReviews(@CurrentUser UserPrincipal user) {
+        ReviewPreviewsResponse reviewsResponse = reviewService.getReviewsByUser(user.getId());
         return new ResponseEntity<>(reviewsResponse, HttpStatus.OK);
     }
 }
