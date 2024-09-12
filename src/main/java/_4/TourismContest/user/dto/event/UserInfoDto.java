33cd121bd66d26eb42d708ed82c276c9fe9002f8
JongKyeong Kim <oo7bangjk@naver.com>
2024-09-13 02:02:55 +0900

    fix: 리뷰 작성자 정보 팬팀 정보 로고로 바꿔서 주기

--- src/main/java/_4/TourismContest/user/dto/event/UserInfoDto.java
+++ src/main/java/_4/TourismContest/user/dto/event/UserInfoDto.java
@@ -3,6 +3,8 @@ package _4.TourismContest.user.dto.event;
 import _4.TourismContest.user.domain.User;
 import lombok.Builder;
 
+import java.util.Map;
+
 @Builder
 public record UserInfoDto(
         Long userId,
@@ -12,11 +14,38 @@ public record UserInfoDto(
 ) {
 
     public static UserInfoDto of(User user) {
+        Map<String, String> teamLogoMap = Map.of(
+                "두산", "Doosan.png",
+                "LG", "LGTwins.png",
+                "KT", "KtWizs.png",
+                "SSG", "SSGLanders.png",
+                "NC", "NCDinos.png",
+                "KIA", "KIA.png",
+                "롯데", "Lotte.png",
+                "삼성", "Samsung.png",
+                "한화", "Hanwha.png",
+                "키움", "Kiwoom.png"
+        );
+        String baseUrl = "https://yaguhang.kro.kr:8443/teamLogos/";
+        String fanTeam;
+        if(user.getFanTeam() == null || user.getFanTeam().equals(""))
+            fanTeam = baseUrl + "main.svg";
+        else{
+            String logoFileName = teamLogoMap.get(user.getFanTeam());
+
+            if (logoFileName == null) {
+                throw new IllegalArgumentException("Unknown team: " + user.getFanTeam() + ". Please check the team name.");
+            }
+
+            fanTeam = baseUrl + logoFileName;
+        }
+
+
         return new UserInfoDto(
                 user.getId(),
                 user.getNickname(),
                 user.getProfileImg(),
-                user.getFanTeam()
+                fanTeam
         );
     }
 }
