ce3c98031e9a337c546764da4b94dac1c13278ee
m0304s <cms8955@naver.com>
2024-09-17 10:16:49 +0900

    Fix:선수맛집 상세페이지 조회 오류 해결

--- src/main/java/_4/TourismContest/spot/repository/AthletePickSpotRepository.java
+++ src/main/java/_4/TourismContest/spot/repository/AthletePickSpotRepository.java
@@ -5,9 +5,12 @@ import _4.TourismContest.spot.domain.Spot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AthletePickSpotRepository extends JpaRepository<AthletePickSpot, Long> {
     List<AthletePickSpot> findAthletePickSpotsBySpotIn(List<Spot> spots);
+    Optional<AthletePickSpot> findAthletePickSpotBySpot(Spot spot);
 }
