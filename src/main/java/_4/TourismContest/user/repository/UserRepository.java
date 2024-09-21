5df71732b6928eceb2afb43b85d3cdda49069b56
FhRh <yiyop@naver.com>
2024-09-21 16:41:39 +0900

    fix : 유저를 email과 provider로 중복검사

--- src/main/java/_4/TourismContest/user/repository/UserRepository.java
+++ src/main/java/_4/TourismContest/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package _4.TourismContest.user.repository;
 
+import _4.TourismContest.oauth.domain.AuthProvider;
 import _4.TourismContest.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Boolean existsByEmail(String email);
+    Boolean existsByEmailAndProvider(String email, AuthProvider provider);
 }
