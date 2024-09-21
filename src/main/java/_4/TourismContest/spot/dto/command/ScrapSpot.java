87bf414ffd7842e99ceae698c0b0674a59d14081
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-09-21 16:38:53 +0900

    fix: 마이페이지 스크랩 api 수정 (#245)

--- src/main/java/_4/TourismContest/spot/dto/command/ScrapSpot.java
+++ src/main/java/_4/TourismContest/spot/dto/command/ScrapSpot.java
@@ -1,9 +1,11 @@
 package _4.TourismContest.spot.dto.command;
 
+import _4.TourismContest.stadium.dto.StadiumInfo;
 import lombok.Builder;
 
 @Builder
 public record ScrapSpot(
+        StadiumInfo stadiumInfo,
         Long contentId,
         String image,
         String title
--- src/main/java/_4/TourismContest/spot/presentation/SpotScrapController.java
+++ src/main/java/_4/TourismContest/spot/presentation/SpotScrapController.java
@@ -25,10 +25,20 @@ public class SpotScrapController {
         return new ResponseEntity<>(spotService.scrapSpot(contentId, stadiumId, userPrincipal), HttpStatus.OK);
     }
 
-    @GetMapping("/spot")
-    @Operation(summary = "내가 스크랩한 스크랩 가져오기" , description = "토큰만 보내면 됩니다.")
-    public ResponseEntity<ScrapResponseDto> scrapSpot(@CurrentUser UserPrincipal userPrincipal){
-        ScrapResponseDto scrapResponseDto = spotService.getMyScrap(userPrincipal);
+//    @GetMapping("/spot")
+//    @Operation(summary = "내가 스크랩한 스크랩 가져오기" , description = "토큰만 보내면 됩니다.")
+//    public ResponseEntity<ScrapResponseDto> scrapSpot(@CurrentUser UserPrincipal userPrincipal){
+//        ScrapResponseDto scrapResponseDto = spotService.getMyScrap(userPrincipal);
+//        return new ResponseEntity<>(scrapResponseDto, HttpStatus.OK);
+//    }
+
+    @GetMapping("/spot/filter")
+    @Operation(summary = "내가 스크랩한 스크랩 필터로 가져오기" , description = "pageIndex, pagesige 넣어주세요. 토큰 필수, 필터 : 전체 , 사직, 고척...")
+    public ResponseEntity<ScrapResponseDto> scrapSpotByFilter(@RequestParam(defaultValue = "0") Integer pageIndex,
+                                                              @RequestParam(defaultValue = "10") Integer pageSize,
+                                                              @RequestParam(defaultValue = "전체") String filter,
+                                                              @CurrentUser UserPrincipal userPrincipal){
+        ScrapResponseDto scrapResponseDto = spotService.getMyScrap(pageIndex, pageSize, filter,userPrincipal);
         return new ResponseEntity<>(scrapResponseDto, HttpStatus.OK);
     }
 }
