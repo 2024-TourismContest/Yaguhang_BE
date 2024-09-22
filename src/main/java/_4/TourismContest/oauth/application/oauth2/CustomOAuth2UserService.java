aaacc6ebc5aadd923ea3850d55faaff9c1946ebc
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-09-22 22:25:32 +0900

    Fix/#243 1 (#249)
    * fix: 추천행 작성 시 spot category 이미지 추가
    
    * fix: 로그인 에러, 이미지 url 경로 설정

--- src/main/java/_4/TourismContest/oauth/application/oauth2/CustomOAuth2UserService.java
+++ src/main/java/_4/TourismContest/oauth/application/oauth2/CustomOAuth2UserService.java
@@ -46,7 +46,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
         }
 
-        Optional<User> userOptional = userRepository.findByEmailAndProvider(oAuth2UserInfo.getEmail(), AuthProvider.KAKAO);
+        Optional<User> userOptional = userRepository.findByEmailAndProvider(oAuth2UserInfo.getEmail(), AuthProvider.kakao);
         User user;
         if(userOptional.isPresent()) {
             user = userOptional.get();
