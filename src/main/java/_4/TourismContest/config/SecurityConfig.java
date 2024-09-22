2877634c81741c124f37cce062ea9465cc347be2
FhRh <yiyop@naver.com>
2024-09-23 03:12:27 +0900

    refact : 사용하지 않는 빈생성 메서드 제거

--- src/main/java/_4/TourismContest/config/SecurityConfig.java
+++ src/main/java/_4/TourismContest/config/SecurityConfig.java
@@ -52,18 +52,6 @@ public class SecurityConfig {
         return new TokenAuthenticationFilter();
     }
 
-
-    /*
-      By default, Spring OAuth2 uses HttpSessionOAuth2AuthorizationRequestRepository to save
-      the authorization request. But, since our service is stateless, we can't save it in
-      the session. We'll save the request in a Base64 encoded cookie instead.
-    */
-//    @Bean
-//    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
-//        return new HttpCookieOAuth2AuthorizationRequestRepository();
-//    }
-
-
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
