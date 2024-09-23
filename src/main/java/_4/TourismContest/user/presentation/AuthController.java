ca0cbb18bedda757a3990b541d7ccdc075b4442b
Due_it <48638700+FhRh@users.noreply.github.com>
2024-09-24 00:24:39 +0900

    [Refact] 사용하지 않는 의존성 제거 (#263)
    * fix : 내가 작성한 리뷰 조회시 이미지를 목록으로 조회한다.
    
    * refact : 사용하지 않는 의존성 제거

--- src/main/java/_4/TourismContest/user/presentation/AuthController.java
+++ src/main/java/_4/TourismContest/user/presentation/AuthController.java
@@ -16,7 +16,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
