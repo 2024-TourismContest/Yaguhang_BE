089e8442ded20d2841c8b8ae286e09a53ad99383
JongKyeong Kim <oo7bangjk@naver.com>
2024-09-30 15:03:39 +0900

    feat: 메일 알림 기능 추가

---
+++ src/main/java/_4/TourismContest/email/application/EmailService.java
@@ -0,0 +1,37 @@
+package _4.TourismContest.email.application;
+
+import _4.TourismContest.exception.BadRequestException;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    public void sendEmail(String toEmail,
+                          String title,
+                          String text) {
+        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        try {
+            emailSender.send(emailForm);
+        } catch (RuntimeException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    // 발신할 이메일 데이터 세팅
+    private SimpleMailMessage createEmailForm(String toEmail,
+                                              String title,
+                                              String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+
+        return message;
+    }
+}
