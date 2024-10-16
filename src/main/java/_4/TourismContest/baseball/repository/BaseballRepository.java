ff288e1ef5feb73dd876b1157e440bf04fa6ae84
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2024-10-16 19:55:02 +0900

    Hot fix (#304)
    * HotFix
    
    * fix : 선수픽 맛집에서 랜덤으로 4개의 요소를 골라 반환한다. 이 때, 4개 이하면 전체를 반환한다.
    
    * fix : 선수픽 맛집에서 랜덤으로 4개의 요소를 골라 반환한다. 이 때, 4개 이하면 전체를 반환한다.
    
    * Fix: 마이페이지 프로필 이미지 null 처리
    
    * Fix: 배포
    
    * Fix: 프로필 이미지 NULL 처리
    
    * Fix: 카테고리 이미지 반환
    
    * Fix: 카테고리 이미지 오류 해결
    
    * Fix: 날씨 이미지 NULL 처리
    
    * Fix: 추천행 상세페이지 카테고리 로고, 카테고리 값 반환
    
    * Fix: 카테고리 한글값으로 반환
    
    * Fix: 엔티티 수정
    
    * Fix: 경기 데이터 출력
    
    * Fix:내가 스크랩한 경기 날짜 수정
    
    * Fix: 변수명 수정
    
    * PostSeason 적용
    
    * HotFix : 경기 조회 날짜 수정
    
    * Fix: 경기 일정 날짜 순으로 정렬
    
    ---------
    
    Co-authored-by: FhRh <yiyop@naver.com>

--- src/main/java/_4/TourismContest/baseball/repository/BaseballRepository.java
+++ src/main/java/_4/TourismContest/baseball/repository/BaseballRepository.java
@@ -17,12 +17,12 @@ public interface BaseballRepository extends JpaRepository<Baseball,Long> {
     @Query("SELECT b FROM Baseball b WHERE b.time = :time AND b.home = :home AND b.away = :away AND b.location = :location")
     Optional<Baseball> findByTimeAndHomeAndAwayAndLocation(@Param("time") LocalDateTime time, @Param("home") String home, @Param("away") String away, @Param("location") String location);
 
-    Page<Baseball> findByStatusNotAndTimeIsAfter(String status,LocalDateTime start, Pageable pageable);
+    Page<Baseball> findByStatusNotAndTimeIsAfterOrderByTime(String status,LocalDateTime start, Pageable pageable);
 
     Optional<Baseball> findFirstByTimeIsAfterOrderByTimeAsc(LocalDateTime start);
 
-    @Query("SELECT b FROM Baseball b WHERE b.status <> :status AND b.time > :start AND (b.home = :team OR b.away = :team)")
-    Page<Baseball> findByStatusNotAndTimeIsAfterAndHomeOrAway(@Param("status") String status, @Param("start") LocalDateTime start, @Param("team") String team, Pageable pageable);
+    @Query("SELECT b FROM Baseball b WHERE b.status <> :status AND b.time > :start AND (b.home = :team OR b.away = :team) order by b.time")
+    Page<Baseball> findByStatusNotAndTimeIsAfterAndHomeOrAwayOrderByTime(@Param("status") String status, @Param("start") LocalDateTime start, @Param("team") String team, Pageable pageable);
 
     boolean existsByTimeBetween(LocalDateTime startOfDay, LocalDateTime endOfDay);
 
