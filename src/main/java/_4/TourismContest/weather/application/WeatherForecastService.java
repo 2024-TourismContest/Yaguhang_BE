e91a17b59c9559b5307b4f93cf4916b2280bbdd4
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-03-03 12:43:24 +0900

    Hot fix (#305)
    * HotFix
    
    * fix : 선수픽 맛집에서 랜덤으로 4개의 요소를 골라 반환한다. 이 때, 4개 이하면 전체를 반환한다.
    
    * fix : 선수픽 맛집에서 랜덤으로 4개의 요소를 골라 반환한다. 이 때, 4개 이하면 전체를 반환한다.
    
    * Fix: 마이페이지 프로필 이미지 null 처리
    
    * Fix: 배포
    
    * Fix: 프로필 이미지 NULL 처리
    
    * Fix: 카테고리 이미지 반환
    
    * Fix: 카테고리 이미지 오류 해결
    
    * Fix: 날씨 이미지 NULL 처리
    
    * Fix: 추천행 상세페이지 카테고리 로고, 카테고리 값 반환
    
    * Fix: 카테고리 한글값으로 반환
    
    * Fix: 엔티티 수정
    
    * Fix: 경기 데이터 출력
    
    * Fix:내가 스크랩한 경기 날짜 수정
    
    * Fix: 변수명 수정
    
    * PostSeason 적용
    
    * HotFix : 경기 조회 날짜 수정
    
    * Fix: 경기 일정 날짜 순으로 정렬
    
    ---------
    
    Co-authored-by: FhRh <yiyop@naver.com>

--- src/main/java/_4/TourismContest/weather/application/WeatherForecastService.java
+++ src/main/java/_4/TourismContest/weather/application/WeatherForecastService.java
@@ -54,7 +54,7 @@ public class WeatherForecastService {
      * @return
      */
     public Page<WeatherForecast> findWeatherForecastDataPerHour(String stadium, int page, int size) {
-        Stadium stadiumEntity = stadiumRepository.findTopByName(stadium)
+        Stadium stadiumEntity = stadiumRepository.findTopByNameContaining(stadium)
                 .orElseThrow(() -> new IllegalArgumentException("Illegal Stadium Name"));
 
         LocalDateTime now = LocalDateTime.now().minusHours(1L);
@@ -80,7 +80,7 @@ public class WeatherForecastService {
         LocalDateTime gameTime = game.getTime().minusHours(1L);
         LocalDateTime endOfDay = game.getTime().toLocalDate().atTime(LocalTime.MAX);
 
-        Stadium stadium = stadiumRepository.findTopByName(game.getLocation())
+        Stadium stadium = stadiumRepository.findTopByNameContaining(game.getLocation())
                 .orElseThrow(() -> new IllegalArgumentException("Illegal Stadium Name"));
 
         Pageable pageable = PageRequest.of(page, size);
@@ -204,7 +204,7 @@ public class WeatherForecastService {
     }
 
     private Stadium getStadiumByName(String location) {
-        return stadiumRepository.findTopByName(location)
+        return stadiumRepository.findTopByNameContaining(location)
                 .orElseThrow(() -> new IllegalArgumentException("Illegal Stadium Name"));
     }
 
@@ -287,7 +287,7 @@ public class WeatherForecastService {
      * @return
      */
     public WeatherForecastEnum getWeatherForecastDataWithGame(Baseball game) {
-        Stadium stadium = stadiumRepository.findTopByName(game.getLocation())
+        Stadium stadium = stadiumRepository.findTopByNameContaining(game.getLocation())
                 .orElseThrow(() -> new IllegalArgumentException("Illegal Stadium Name"));
 
         LocalDateTime gameTime = game.getTime();
